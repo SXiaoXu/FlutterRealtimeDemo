b6e9f4c8214471ae69e44ad1789f1977839b38d5
xsui <284898541@qq.com>
2020-09-10 23:01:57 +0800

    add android mixpush

--- lc_realtime/android/app/src/main/java/com/example/lcrealtime/MainActivity.java
+++ lc_realtime/android/app/src/main/java/com/example/lcrealtime/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lcrealtime;
 
 import androidx.annotation.NonNull;
+
+import cn.leancloud.AVMixPushManager;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
@@ -8,6 +10,7 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 public class MainActivity extends FlutterActivity {
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    AVMixPushManager.connectHMS(this);
     GeneratedPluginRegistrant.registerWith(flutterEngine);
   }
 }
--- lc_realtime/android/app/src/main/java/com/example/lcrealtime/MyApplication.java
+++ lc_realtime/android/app/src/main/java/com/example/lcrealtime/MyApplication.java
@@ -1,5 +1,6 @@
 package com.example.lcrealtime;
 import cn.leancloud.AVLogger;
+import cn.leancloud.AVMixPushManager;
 import cn.leancloud.AVOSCloud;
 import cn.leancloud.im.AVIMOptions;
 import io.flutter.app.FlutterApplication;
@@ -8,7 +9,11 @@ public class MyApplication extends FlutterApplication{
     private static final String LC_App_Id = "1eUivazFXYwJvuGpPl2LE4uY-gzGzoHsz";
     private static final String LC_App_Key = "nLMIaQSwIsHfF206PnOFoYYa";
     private static final String LC_Server_Url = "https://1euivazf.lc-cn-n1-shared.com";
+    private static final String MEIZU_APP = "119851";
+    private static final String MEIZU_KEY = "73c48ba926fb40b898797b819030830d";
 
+    private static final String MI_APPID = "2882303761517988199";
+    private static final String MI_APPKEY = "5571798886199";
     @Override
     public void onCreate() {
         super.onCreate();
@@ -17,5 +22,13 @@ public class MyApplication extends FlutterApplication{
 
         AVOSCloud.setLogLevel(AVLogger.Level.DEBUG);
         AVOSCloud.initialize(this, LC_App_Id, LC_App_Key, LC_Server_Url);
+
+        //华为推送
+        AVMixPushManager.registerHMSPush(this);
+        //小米
+        AVMixPushManager.registerXiaomiPush(this , MI_APPID, MI_APPKEY);
+        //魅族
+        AVMixPushManager.registerFlymePush(this, MEIZU_APP, MEIZU_KEY);
+
     }
 }
