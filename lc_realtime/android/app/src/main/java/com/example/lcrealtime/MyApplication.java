11e7847fda4703764faacf7b7aa445743488de1b
xsui <284898541@qq.com>
2021-12-20 18:53:52 +0800

    update android sdk version

--- lc_realtime/android/app/src/main/java/com/example/lcrealtime/MyApplication.java
+++ lc_realtime/android/app/src/main/java/com/example/lcrealtime/MyApplication.java
@@ -1,33 +1,33 @@
 package com.example.lcrealtime;
-import cn.leancloud.AVLogger;
-import cn.leancloud.AVOSCloud;
-import cn.leancloud.im.AVIMOptions;
+import cn.leancloud.LCLogger;
+import cn.leancloud.LeanCloud;
+import cn.leancloud.im.LCIMOptions;
 import io.flutter.app.FlutterApplication;
 
 public class MyApplication extends FlutterApplication{
     private static final String LC_App_Id = "1eUivazFXYwJvuGpPl2LE4uY-gzGzoHsz";
     private static final String LC_App_Key = "nLMIaQSwIsHfF206PnOFoYYa";
     private static final String LC_Server_Url = "https://1euivazf.lc-cn-n1-shared.com";
-    private static final String MEIZU_APP = "119851";
-    private static final String MEIZU_KEY = "73c48ba926fb40b898797b819030830d";
-
-    private static final String MI_APPID = "2882303761517988199";
-    private static final String MI_APPKEY = "5571798886199";
+//    private static final String MEIZU_APP = "119851";
+//    private static final String MEIZU_KEY = "73c48ba926fb40b898797b819030830d";
+//
+//    private static final String MI_APPID = "2882303761517988199";
+//    private static final String MI_APPKEY = "5571798886199";
     @Override
     public void onCreate() {
         super.onCreate();
 
-        AVIMOptions.getGlobalOptions().setUnreadNotificationEnabled(true);
+        LCIMOptions.getGlobalOptions().setUnreadNotificationEnabled(true);
 
-        AVOSCloud.setLogLevel(AVLogger.Level.DEBUG);
-        AVOSCloud.initialize(this, LC_App_Id, LC_App_Key, LC_Server_Url);
+        LeanCloud.setLogLevel(LCLogger.Level.DEBUG);
+        LeanCloud.initialize(this, LC_App_Id, LC_App_Key, LC_Server_Url);
 
 //        //华为推送
-//        AVMixPushManager.registerHMSPush(this);
+//        LCMixPushManager.registerHMSPush(this);
 //        //小米
-//        AVMixPushManager.registerXiaomiPush(this , MI_APPID, MI_APPKEY);
+//        LCMixPushManager.registerXiaomiPush(this , MI_APPID, MI_APPKEY);
 //        //魅族
-//        AVMixPushManager.registerFlymePush(this, MEIZU_APP, MEIZU_KEY);
+//        LCMixPushManager.registerFlymePush(this, MEIZU_APP, MEIZU_KEY);
 
     }
 }
